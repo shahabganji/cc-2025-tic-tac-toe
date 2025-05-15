34520bed4ab52c6162f2d067cd39d38f79ad82e6
Shahab <shahabganji@gmail.com>
2025-05-15 23:08:44 +0200

    docs: improve XML documentation for EventStream.Get method
    Enhanced the XML comments for the `Get` method in `EventStream` to provide a clearer explanation of its functionality, parameters, and return value. The updated description outlines the purpose of the method, behavior of the `ignoreUnknownEvents` flag, and the expected result of the operation.

--- src/TicTacToe/Domain/Shared/EventStream.cs
+++ src/TicTacToe/Domain/Shared/EventStream.cs
@@ -5,12 +5,16 @@ public sealed class EventStream<T>(Guid streamId, IEventStore store) where T : E
     public long Version => store.Version;
 
     /// <summary>
-    /// 
+    /// Retrieves an instance of the aggregate root of type <typeparamref name="T"/> by loading its
+    /// event stream and applying them to the aggregate. Optionally, unknown events can be ignored.
     /// </summary>
-    /// <param name="ignoreUnknownEvents">This helps
-    /// to create aggregates that are based on the same stream
-    /// but ignoring some of the events that are not relevant to them</param>
-    /// <returns></returns>
+    /// <param name="ignoreUnknownEvents">
+    /// If set to true, any unknown events in the stream will be ignored during the reconstruction of the aggregate.
+    /// If false, encountering an unknown event will throw an exception.
+    /// </param>
+    /// <returns>
+    /// The reconstructed aggregate root of type <typeparamref name="T"/>, or null if no events were found for the stream.
+    /// </returns>
     public async Task<T?> Get(bool ignoreUnknownEvents = false)
     {
         var events = await store.LoadStreamEvents(streamId);
